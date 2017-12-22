cd53d0e9c7a20150ba7e717755a567eeed867d34
Javen King <javen_king@qq.com>
2017-12-22 15:16:22 +0800

    init project

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,17 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, titleText) {
+    'use strict'
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'user strict'
+    return thumbnail.getAttribute('data-image-url');
+}
