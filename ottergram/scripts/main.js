40850a681a75e62ea2fe7d2614e4b2066c0360d5
JinXin1995 <jinxin807507011@outlook.com>
2017-12-29 11:11:16 +0800

    complete ottergram

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var TINY_EFFECT_CLASS = "is-tiny";
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict'
@@ -12,6 +16,63 @@ function setDetails(imageUrl, titleText) {
 }
 
 function imageFromThumb(thumbnail) {
-    'user strict'
+    'use strict'
     return thumbnail.getAttribute('data-image-url');
 }
+
+function titleFromThumb(thumbnail) {
+    'use strict'
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict'
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHander(thumb) {
+    'use strict'
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails();
+    })
+}
+
+function getThumbnailsArray() {
+    'use strict'
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailsArray = [].slice.call(thumbnails);
+    return thumbnailsArray;
+}
+
+function hiddenDetails() {
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function addKeyPressHander() {
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        if (event.keyCode === ESC_KEY) {
+            hiddenDetails();
+        }
+    })
+}
+
+function initializeEvents() {
+    'use strict'
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHander);
+    addKeyPressHander();
+}
+
+initializeEvents();
